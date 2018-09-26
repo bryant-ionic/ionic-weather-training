97bc2d3d691162818c76c00d682c29de6dc602ef
Bryant Plano <bryant@ionic.io>
2018-09-26 15:49:30 -0500

    Finishing Step 06

---
+++ src/models/weather.ts
@@ -0,0 +1,6 @@
+export interface Weather {
+    temperature: number;
+    condition: number;
+    date?: Date;
+}
+  
--- src/pages/current-weather/current-weather.ts
+++ src/pages/current-weather/current-weather.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { IconMapProvider } from '../../providers/icon-map/icon-map';
+import { Weather } from '../../models/weather';
+
 @Component({
   selector: 'page-current-weather',
   templateUrl: 'current-weather.html'
 })
 export class CurrentWeatherPage {
+  currentWeather: Weather = {
+    temperature: 302,
+    condition: 200
+  };
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public iconMap: IconMapProvider) {}
 }
